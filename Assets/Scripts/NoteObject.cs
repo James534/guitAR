92cdbf1bc06a1807c9320fbe921d24996b63f411
Wenxin Chen <wenxinchen11@hotmail.com>
2016-08-07 12:07:45 -0400

    added fret markings

--- Assets/Scripts/NoteObject.cs
+++ Assets/Scripts/NoteObject.cs
@@ -1,20 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NoteObject : MonoBehaviour {
+public class NoteObject : MonoBehaviour
+{
 
-    float speed = 0.05f;
+    float speed = 0.1f;
     public bool done = false;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         Vector3 pos = transform.position;
         pos.x -= speed;
+        pos.y += speed;
         if (pos.x < 8)
         {
             done = true;
@@ -22,5 +26,5 @@ public class NoteObject : MonoBehaviour {
         }
 
         transform.position = pos;
-	}
+    }
 }
--- Assets/Scripts/SongPlayer.cs
+++ Assets/Scripts/SongPlayer.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class SongPlayer : MonoBehaviour {
+public class SongPlayer : MonoBehaviour
+{
 
     List<List<Note>> tabs;
     const float BEAT_TO_SEC = 0.5f;
@@ -15,21 +16,24 @@ public class SongPlayer : MonoBehaviour {
     float deltaT = 0;
     public float zoffset = 33, xoffset = 20;
 
-    bool done = false;
+    public bool done = false;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Starting song player!");
         tabs = SongLibary.getJingleBells();
         tabObjects = new GameObject[6];
         for (int i = 0; i < 6; i++)
             //tabObjects[i] = (GameObject)Resources.Load("String"+(i+1));
-            tabObjects[i] = (GameObject)Resources.Load(""+(i + 1));
+            tabObjects[i] = (GameObject)Resources.Load("" + (i + 1));
 
         currentNotes = new List<GameObject>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         for (int i = 0; i < currentNotes.Count; i++)
         {
             GameObject toDelete = currentNotes[i];
@@ -50,18 +54,18 @@ public class SongPlayer : MonoBehaviour {
             for (int i = 0; i < tabs[tabIndex].Count; i++)
             {
                 Note n = tabs[tabIndex][i];
-                GameObject newNote = Instantiate(tabObjects[n.strNum-1]);
-                Vector3 pos = new Vector3(xoffset, 0, zoffset - (-1 + n.tabNum * 3.5f));
+                GameObject newNote = Instantiate(tabObjects[n.strNum - 1]);
+                Vector3 pos = new Vector3(xoffset, -25.54f, zoffset - (-1 + n.tabNum * 3.5f));
                 newNote.transform.position = pos;
                 currentNotes.Add(newNote);
                 deltaT = n.dur * BEAT_TO_SEC;
             }
             tabIndex++;
-            if (tabIndex > tabs.Count-1)
+            if (tabIndex > tabs.Count - 1)
             {
                 done = true;
                 Debug.Log("Done song!");
             }
         }
-	}
+    }
 }
